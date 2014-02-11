736dff5fc2424589443e5a5e5f1c3be42a06af62
Justin Eng <engjustin3@gmail.com>
2014-02-10 23:22:13 -0800

    Started 11507

---
+++ src/p11507.java
@@ -0,0 +1,52 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+/**
+ * Created by Justin on 2/10/14.
+ */
+public class p11507 {
+    public static void main(String[] args) {
+        InputStream inputStream = System.in;
+        OutputStream outputStream = System.out;
+        InputReader in = new InputReader(inputStream);
+        PrintWriter out = new PrintWriter(outputStream);
+        Solver solver = new Solver();
+        solver.solve(in, out);
+    }
+
+    private static class Solver {
+        public void solve(InputReader in, PrintWriter out) {
+            System.out.println("Hello world!");
+        }
+    }
+
+    private static class InputReader {
+        public BufferedReader reader;
+        public StringTokenizer tokenizer;
+
+        public InputReader(InputStream stream) {
+            reader = new BufferedReader(new InputStreamReader(stream));
+            tokenizer = null;
+        }
+
+        public String next() {
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    String line = reader.readLine();
+                    tokenizer = new StringTokenizer(line);
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return tokenizer.nextToken();
+        }
+
+        public int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        public double nextDouble() {
+            return Double.parseDouble(next());
+        }
+    }
+}
