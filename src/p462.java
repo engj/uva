4c0a6a11c5e72a2b5b7daa5899be95ea65aad3a8
Justin Eng <engjustin3@gmail.com>
2014-02-15 15:27:23 -0800

    Finished 462.

--- src/p462.java
+++ src/p462.java
@@ -2,7 +2,7 @@ import java.io.*;
 import java.util.StringTokenizer;
 
 /**
- * Created by Justin on 2/14/14.
+ * Created by Justin on 2/15/14.
  */
 public class p462 {
     public static void main(String[] args) {
@@ -17,96 +17,173 @@ public class p462 {
     private static class Solver {
         public void solve(InputReader in, PrintWriter out) {
             while(true) {
-                int p = 0;
-                int[] hR = new int[13];
-                int[] hS = new int[4];
-                char[] su = new char[4];
-                su[0] = 'S'; su[1] = 'H'; su[2] = 'D'; su[3] = 'C';
-                char[] stp = new char[4];
-                boolean[] stpp = new boolean[4];
-
                 String line;
                 try {
                     line = in.reader.readLine();
-                } catch (IOException e) {
+                } catch (IOException e ) {
                     throw new RuntimeException(e);
                 }
                 StringTokenizer st = new StringTokenizer(line);
+
+                int[][] h = new int[4][13];
+                int p = 0;
+                boolean[] stp = new boolean[4];
+                boolean allStp = true;
+                char[] su = new char[4];
+                su[0] = 'S'; su[1] = 'H'; su[2] = 'D'; su[3] = 'C';
+                char bstBd = '0';
+                int mstCrds = 0;
 
                 while (st.hasMoreTokens()) {
                     String c = st.nextToken();
                     char r = c.charAt(0);
                     char s = c.charAt(1);
 
-                    if (s == 'S') hS[0]++;
-                    else if (s == 'H') hS[1]++;
-                    else if (s == 'D') hS[2]++;
-                    else if (s == 'C') hS[3]++;
-
                     if (r == 'A') {
                         p += 4;
-                        hR[0]++;
-                        if (s == 'S') stp[0] = 'A';
-                        else if (s == 'H') stp[1] = 'A';
-                        else if (s == 'D') stp[2] = 'A';
-                        else if (s == 'C') stp[3] = 'A';
-                    } else if (r == 'K') {
-                        p += 3;
-                        hR[12]++;
-                        if (s == 'S') stp[0] = 'K';
-                        else if (s == 'H') stp[1] = 'K';
-                        else if (s == 'D') stp[2] = 'K';
-                        else if (s == 'C') stp[3] = 'K';
-                    } else if (r == 'Q') {
-                        p += 2;
-                        hR[11]++;
-                        if (s == 'S') stp[0] = 'Q';
-                        else if (s == 'H') stp[1] = 'Q';
-                        else if (s == 'D') stp[2] = 'Q';
-                        else if (s == 'C') stp[3] = 'Q';
+                        if (s == 'S') h[0][0] = 1;
+                        else if (s == 'H') h[1][0] = 1;
+                        else if (s == 'D') h[2][0] = 1;
+                        else if (s == 'C') h[3][0] = 1;
                     } else if (r == 'J') {
                         p += 1;
-                        hR[10]++;
+                        if (s == 'S') h[0][10] = 1;
+                        else if (s == 'H') h[1][10] = 1;
+                        else if (s == 'D') h[2][10] = 1;
+                        else if (s == 'C') h[3][10] = 1;
+                    } else if (r == 'Q') {
+                        p += 2;
+                        if (s == 'S') h[0][11] = 1;
+                        else if (s == 'H') h[1][11] = 1;
+                        else if (s == 'D') h[2][11] = 1;
+                        else if (s == 'C') h[3][11] = 1;
+                    } else if (r == 'K') {
+                        p += 3;
+                        if (s == 'S') h[0][12] = 1;
+                        else if (s == 'H') h[1][12] = 1;
+                        else if (s == 'D') h[2][12] = 1;
+                        else if (s == 'C') h[3][12] = 1;
                     } else if (r == 'T') {
-                        hR[9]++;
+                        if (s == 'S') h[0][9] = 1;
+                        else if (s == 'H') h[1][9] = 1;
+                        else if (s == 'D') h[2][9] = 1;
+                        else if (s == 'C') h[3][9] = 1;
                     } else {
-                        hR[Character.getNumericValue(r) - 1]++;
+                        int cN = Character.getNumericValue(r) - 1;
+                        if (s == 'S') h[0][cN] = 1;
+                        else if (s == 'H') h[1][cN] = 1;
+                        else if (s == 'D') h[2][cN] = 1;
+                        else if (s == 'C') h[3][cN] = 1;
                     }
                 }
 
-                if (hR[12] == 1) p--;
-                if (hR[11] == 1 || hR[11] == 2) p--;
-                if (hR[10] == 1 || hR[10] == 2 || hR[10] == 3) p--;
-
+                /*
                 for (int i = 0; i < 4; i++) {
-                    if (stp[i] == 'A') stpp[i] = true;
-                    if (stp[i] == 'K' && hS[i] >= 2) stpp[i] = true;
-                    if (stp[i] == 'Q' && hS[i] >= 3) stpp[i] = true;
+                    for (int j = 0; j < 13; j++) {
+                        System.out.print(h[i][j]);
+                        System.out.print(' ');
+                    }
+                    System.out.println();
+                }
+                */
+
+                // rule 2
+                for (int i = 0; i < 4; i++) {
+                    if (h[i][12] == 1) {
+                        int j;
+                        for (j = 0; j < 13; j++) {
+                            if (h[i][j] == 1 && j != 12)
+                                break;
+                        }
+                        if (j == 13)
+                            p -= 1;
+                    }
                 }
 
-                boolean allStopped = true;
+                // rule 3
                 for (int i = 0; i < 4; i++) {
-                    if (stpp[i] == false) allStopped = false;
+                    if (h[i][11] == 1) {
+                        int k = 0;
+                        for (int j = 0; j < 13; j++)
+                            if (h[i][j] == 1 && j != 11)
+                                k++;
+                        if (k <= 1)
+                            p -= 1;
+                    }
                 }
 
-                if (p >= 16 && allStopped == true) {
+                // rule 4
+                for (int i = 0; i < 4; i++) {
+                    if (h[i][10] == 1) {
+                        int k = 0;
+                        for (int j = 0; j < 13; j++)
+                            if (h[i][j] == 1 && j != 10)
+                                k++;
+                        if (k <= 2)
+                            p -= 1;
+                    }
+                }
+
+                // no-trump
+                for (int i = 0; i < 4; i++) {
+                    if (h[i][0] == 1)
+                        stp[i] = true;
+                    if (h[i][12] == 1) {
+                        int k = 0;
+                        for (int j = 0; j < 13; j++)
+                            if (h[i][j] == 1 && j != 12)
+                                k++;
+                        if (k >= 1)
+                            stp[i] = true;
+                    }
+                    if (h[i][11] == 1) {
+                        int k = 0;
+                        for (int j = 0; j < 13; j++)
+                            if (h[i][j] == 1 && j != 11)
+                                k++;
+                        if (k >= 2)
+                            stp[i] = true;
+                    }
+                }
+
+                for (int i = 0; i < 4; i++)
+                    if (stp[i] == false)
+                        allStp = false;
+
+                if (p >= 16 && allStp == true) {
                     System.out.println("BID NO-TRUMP");
-                    break;
+                    continue;
                 }
 
-                int m = 0;
+                // rules 5, 6, 7
                 for (int i = 0; i < 4; i++) {
-                    if (hS[i] == 2) p += 1;
-                    else if (hS[i] == 1 || hS[i] == 0) p += 2;
-                    if (hS[i] > hS[m]) m = i;
+                    int k = 0;
+                    for (int j = 0; j < 13; j++)
+                        if (h[i][j] == 1)
+                            k++;
+                    if (k == 2)
+                        p += 1;
+                    else if (k == 1)
+                        p += 2;
+                    else if (k == 0)
+                        p += 2;
+                }
+
+                for (int i = 0; i < 4; i++) {
+                    int k = 0;
+                    for (int j = 0; j < 13; j++)
+                        if (h[i][j] == 1)
+                            k++;
+                    if (k > mstCrds) {
+                        mstCrds = k;
+                        bstBd = su[i];
+                    }
                 }
 
                 if (p < 14)
                     System.out.println("PASS");
                 else
-                    System.out.println("BID " + su[m]);
-
-                break;
+                    System.out.println("BID " + bstBd);
             }
         }
     }
