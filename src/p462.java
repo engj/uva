1ab05dbdfbfb96b5139ea274e120710646455546
Justin Eng <engjustin3@gmail.com>
2014-02-15 13:38:24 -0800

    Added changes.

--- src/p462.java
+++ src/p462.java
@@ -22,9 +22,10 @@ public class p462 {
                 int[] hS = new int[4];
                 char[] su = new char[4];
                 su[0] = 'S'; su[1] = 'H'; su[2] = 'D'; su[3] = 'C';
-                boolean[] stp = new boolean[4];
-                String line;
+                char[] stp = new char[4];
+                boolean[] stpp = new boolean[4];
 
+                String line;
                 try {
                     line = in.reader.readLine();
                 } catch (IOException e) {
@@ -37,24 +38,36 @@ public class p462 {
                     char r = c.charAt(0);
                     char s = c.charAt(1);
 
-                    if (s == 'S' || s == 's') hS[0]++;
-                    else if (s == 'H' || s == 'h') hS[1]++;
-                    else if (s == 'D' || s == 'd') hS[2]++;
-                    else if (s == 'C' || s == 'c') hS[3]++;
+                    if (s == 'S') hS[0]++;
+                    else if (s == 'H') hS[1]++;
+                    else if (s == 'D') hS[2]++;
+                    else if (s == 'C') hS[3]++;
 
-                    if (r == 'A' || r == 'a') {
+                    if (r == 'A') {
                         p += 4;
                         hR[0]++;
-                    } else if (r == 'K' || r == 'k') {
+                        if (s == 'S') stp[0] = 'A';
+                        else if (s == 'H') stp[1] = 'A';
+                        else if (s == 'D') stp[2] = 'A';
+                        else if (s == 'C') stp[3] = 'A';
+                    } else if (r == 'K') {
                         p += 3;
                         hR[12]++;
-                    } else if (r == 'Q' || r == 'q') {
+                        if (s == 'S') stp[0] = 'K';
+                        else if (s == 'H') stp[1] = 'K';
+                        else if (s == 'D') stp[2] = 'K';
+                        else if (s == 'C') stp[3] = 'K';
+                    } else if (r == 'Q') {
                         p += 2;
                         hR[11]++;
-                    } else if (r == 'J' || r == 'j') {
+                        if (s == 'S') stp[0] = 'Q';
+                        else if (s == 'H') stp[1] = 'Q';
+                        else if (s == 'D') stp[2] = 'Q';
+                        else if (s == 'C') stp[3] = 'Q';
+                    } else if (r == 'J') {
                         p += 1;
                         hR[10]++;
-                    } else if (r == 'T' || r == 't') {
+                    } else if (r == 'T') {
                         hR[9]++;
                     } else {
                         hR[Character.getNumericValue(r) - 1]++;
@@ -65,7 +78,21 @@ public class p462 {
                 if (hR[11] == 1 || hR[11] == 2) p--;
                 if (hR[10] == 1 || hR[10] == 2 || hR[10] == 3) p--;
 
-                //if (p >= 16 && no suits stopped) {
+                for (int i = 0; i < 4; i++) {
+                    if (stp[i] == 'A') stpp[i] = true;
+                    if (stp[i] == 'K' && hS[i] >= 2) stpp[i] = true;
+                    if (stp[i] == 'Q' && hS[i] >= 3) stpp[i] = true;
+                }
+
+                boolean allStopped = true;
+                for (int i = 0; i < 4; i++) {
+                    if (stpp[i] == false) allStopped = false;
+                }
+
+                if (p >= 16 && allStopped == true) {
+                    System.out.println("BID NO-TRUMP");
+                    break;
+                }
 
                 int m = 0;
                 for (int i = 0; i < 4; i++) {
