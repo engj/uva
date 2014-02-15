6904936e6d60171b9c1473d762a2c2ff18eba87f
Justin Eng <engjustin3@gmail.com>
2014-02-14 20:18:32 -0800

    Worked on 462.

--- src/p462.java
+++ src/p462.java
@@ -16,37 +16,44 @@ public class p462 {
 
     private static class Solver {
         public void solve(InputReader in, PrintWriter out) {
-            int p = 0;
             while(true) {
-
+                int p = 0;
                 int[] h = new int[13];
-                char r = in.next().charAt(0);
-                if (r == 'A') {
-                    p += 4;
-                    h[0]++;
-                } else if (r == 'K') {
-                    p += 3;
-                    h[12]++;
-                } else if (r == 'Q') {
-                    p += 2;
-                    h[11]++;
-                } else if (r == 'J') {
-                    p += 1;
-                    h[10]++;
-                } else if (r== 'T') {
-                    h[9]++;
-                } else {
-                    h[Character.getNumericValue(r) - 1]++;
+                String line;
+                try {
+                    line = in.reader.readLine();
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+                StringTokenizer st = new StringTokenizer(line);
+                while (st.hasMoreTokens()) {
+                    char r = st.nextToken().charAt(0);
+                    if (r == 'A') {
+                        p += 4;
+                        h[0]++;
+                    } else if (r == 'K') {
+                        p += 3;
+                        h[12]++;
+                    } else if (r == 'Q') {
+                        p += 2;
+                        h[11]++;
+                    } else if (r == 'J') {
+                        p += 1;
+                        h[10]++;
+                    } else if (r== 'T') {
+                        h[9]++;
+                    } else {
+                        h[Character.getNumericValue(r) - 1]++;
+                    }
                 }
                 if (h[12] == 1) p--;
                 if (h[11] == 1 || h[11] == 2) p--;
                 if (h[10] == 1 || h[10] == 2 || h[10] == 3) p--;
                 for (int i = 0; i < 13; i++) {
-                    if (h[i] == 2) p++;
-                    if (h[i] == 1) p += 2;
-                    if (h[i] == 0) p +=2;
+                    System.out.println(h[i]);
                 }
                 System.out.println(p);
+                break;
             }
         }
     }
