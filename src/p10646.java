a878dcdd3d0dc0a8ac2a5d4e1e4d4c780d1c845c
Justin Eng <engjustin3@gmail.com>
2014-02-16 16:25:18 -0800

    Finished 10646.

---
+++ src/p10646.java
@@ -0,0 +1,43 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+/**
+ * Created by Justin on 2/15/14.
+ */
+public class p10646 {
+    private static int cardVal(String c) {
+        char r = c.charAt(0);
+        if (r >= '2' && r <= '9') {
+            return Character.getNumericValue(r);
+        }
+        return 10;
+    }
+    public static void main(String[] args) throws Exception {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter out = new PrintWriter(System.out);
+        int n = Integer.parseInt(in.readLine());
+        for (int i = 1; i <= n; i++) {
+            String[] crds = in.readLine().split("\\s+");
+            int top = 26;
+            int y = 0;
+            for (int j = 0; j < 3; j++) {
+                String tC = crds[top];
+                int x = cardVal(tC);
+                y += x;
+                top--;
+                if (x != 10)
+                    top -= 10 - x;
+            }
+            // Did not check for this condition the first time and recieved a WA.
+            if (y <= top) {
+                out.println("Case " + i + ": " + crds[y]);
+            } else {
+                out.println("Case " + i + ": " + crds[25 + y - top]);
+            }
+        }
+        in.close();
+        out.close();
+    }
+}
--- src/test.java
+++ src/test.java
@@ -1,24 +1,6 @@
 import java.io.*;
 import java.util.*;
 public class test {
-    public static void main(String args[] ) throws Exception {
-        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-        PrintWriter out = new PrintWriter(System.out);
-
-        int t = Integer.parseInt(in.readLine());
-        for (int i = 1; i <= t; i++) {
-            StringTokenizer line = new StringTokenizer(in.readLine());
-            int n = Integer.parseInt(line.nextToken());
-            int max = 0;
-            while (line.hasMoreTokens()) {
-                int s = Integer.parseInt(line.nextToken());
-                if (s > max)
-                    max = s;
-            }
-            System.out.println("Case " + i + ":" + " " + max);
-        }
-
-        in.close();
-        out.close();
+    public static void main(String[] args) {
     }
 }
