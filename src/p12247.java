9dfde61b05c28acddf4defe864fc553910ab1c5d
Justin Eng <engjustin3@gmail.com>
2014-02-17 18:32:06 -0800

    Worked on 12247.

--- src/p12247.java
+++ src/p12247.java
@@ -7,6 +7,13 @@ import java.util.StringTokenizer;
  * Created by Justin on 2/16/14.
  */
 public class p12247 {
+    private static int numWins(int[] a, int b) {
+        int n = 0;
+        for (int i = 0; i < 3; i++)
+            if (b > a[i])
+                n++;
+        return n;
+    }
     public static void main(String[] args) throws Exception {
         BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
         PrintWriter out = new PrintWriter(System.out);
@@ -14,9 +21,13 @@ public class p12247 {
         while (true) {
             StringTokenizer line = new StringTokenizer(in.readLine());
             int[] crds = new int[5];
+            int[] crdsTaken = new int[52];
             int numZeros = 0;
             for (int i = 0; i < 5; i++) {
-                crds[i] = Integer.parseInt(line.nextToken());
+                int crd = Integer.parseInt(line.nextToken());
+                crds[i] = crd;
+                if (crd != 0)
+                    crdsTaken[crd - 1] = 1;
                 if (crds[i] == 0)
                     numZeros++;
             }
@@ -24,21 +35,28 @@ public class p12247 {
             if (numZeros == 5)
                 break;
 
-            int[] numBeats = new int[2];
-            for (int i = 3; i < 5; i++)
-                for (int j = 0; j < 3; j++)
-                    if (crds[i] > crds[j])
-                        numBeats[j]++;
+            int numWinsA = numWins(crds, crds[3]);
+            int numWinsB = numWins(crds, crds[4]);
 
-            if (numBeats[0] == 0 || numBeats[1] == 0) {
+            if ((numWinsA < 2 || numWinsB < 2) && !(numWinsA == 3 || numWinsB == 3)) {
                 System.out.println("-1");
                 continue;
             }
 
+            int minAmt = 2;
+            if ((numWinsA < 2 || numWinsB < 2) && (numWinsA == 3 || numWinsB == 3))
+                minAmt = 3;
+            if (numWinsA == 3 && numWinsB == 3)
+                minAmt = 0;
 
-
+            int r = -1;
+            for (int i = 0; i < 52; i++)
+                if (crdsTaken[i] != 1 && numWins(crds, i + 1) >= minAmt) {
+                    r = i + 1;
+                    break;
+                }
+            System.out.println(r);
         }
-
         in.close();
         out.close();
     }
--- src/test.java
+++ src/test.java
@@ -1,6 +1,31 @@
 import java.io.*;
 import java.util.*;
 public class test {
+    private static int[] insertionSort(int[] a) {
+        for (int i = 0; i < a.length; i++) {
+            int temp = a[i];
+            int j;
+            for (j = i - 1; j > 0; j--) {
+                if (a[j] > temp)
+                    a[j + 1] = a[j];
+                else
+                    break;
+            }
+            a[j + 1] = temp;
+        }
+        return a;
+    }
     public static void main(String[] args) {
+        int[] a = new int[6];
+        a[0] = 1;
+        a[1] = 2;
+        a[2] = 3;
+        a[3] = 3;
+        a[4] = 2;
+        a[5] = 1;
+        a = insertionSort(a);
+        for (int i = 0; i < a.length; i++) {
+            System.out.println(a[i]);
+        }
     }
 }
